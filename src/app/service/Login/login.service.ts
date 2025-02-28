2cb8e2686478928f6bcce749dd5e051e2ac2992e
mauricio <mauriciochasi@gmail.com>
2025-02-28 01:16:20 +0100

    url

--- src/app/service/Login/login.service.ts
+++ src/app/service/Login/login.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class LoginService {
 
-  private url= 'https://34.244.212.188:8080/autenticar';
+  private url= 'http://34.244.212.188:8080/autenticar';
 
   constructor(
    private http: HttpClient
